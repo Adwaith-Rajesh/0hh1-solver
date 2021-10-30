cf8103adf5290fa79ac80dd054676eb10e7ee814
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-10-30 09:41:03 +0530

    Added the main parser

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from __future__ import annotations
 import argparse
 from typing import Sequence
 
+from .web import run as web_runner
 
-def main(argv: Sequence[str] | None) -> int:
+
+def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("type", type=str, help="web or android")
+    parser.add_argument("shape", type=int, help="the shape of the puzzle (4, 6, 8, 10, 12)")
 
     args = parser.parse_args(argv)
     print(args)
+
+    if args.type == "web":
+        web_runner(args.shape)
     return 0
 
 
